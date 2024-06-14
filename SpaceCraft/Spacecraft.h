7036b1486e8c32214c7b22083a62061d74afd1bd
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-06-14 08:08:16 +0300

    update spacecraft

--- SpaceCraft/Spacecraft.h
+++ SpaceCraft/Spacecraft.h
@@ -21,18 +21,22 @@ public:
     void shoot(); // Method to shoot projectiles
     void takeDamage(int damage); // Method to decrease health
     void increaseHealth(int healthIncrease); // Method to increase health
+    void increaseScore(int val);
     void setup();
     // Getters
     float getX() const { return xVal; }
     float getZ() const { return zVal; }
     float getAngle() const { return angle; }
     int getHealth() const { return health; }
+    int getScore() const { return score; }
     bool isEnemy() const { return enemy; }
     // Setters
     void setX(float x) { xVal = x; }
     void setZ(float z) { zVal = z; }
     void setAngle(float a) { angle = a; }
     void setHealth(int h) { health = h; }
+    void setScore(int s) { score = s; }
+
     void setEnemy(bool e) { enemy = e; }
     unsigned int texture[2];
     vector<Projectile> projectiles; // Vector to store projectiles
@@ -48,6 +52,7 @@ private:
     float xVal, zVal; // Coordinates of the spacecraft
     float angle; // Angle of the spacecraft
     int health; // Health of the spacecraft
+    int score;
     bool enemy;
     unsigned int spacecraftModel; // Display list for the spacecraft model
 };
