0d7d4065f5ba1f602c84cd4993ccafd77023968f
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-06-15 09:57:26 +0300

    game over sounds and visual effects

--- Game/game.cpp
+++ Game/game.cpp
@@ -9,12 +9,18 @@
 #include "../Asteriods/Asteriods.h"
 #include "../SpaceCraft/SpaceCraft.h"
 #include "../Pickable/Pickable.h"
+#include "../Menu/menu.h"
 #include <vector>
 #include <ctime>
+#include <algorithm>
+#include <thread>
 
 using namespace std;
 
 // Globals.
+extern int windowChoice;
+extern Menu menu;
+
 float aspectRatio; // Aspect ratio of the window
 Planet *planets = nullptr;
 Asteriods *asteriods = nullptr;
@@ -34,7 +40,163 @@ int pickableGenerationInterval = 7000; // Generate new pickables every 10 second
 
 int lastPickableGenerationTime = 0;
 
+float explosionX = 0.0f, explosionZ = 0.0f;
+bool isExplosion = false;
+float explosionRadius = 0.0f, explosionMaxRadius = 8.0f;
+std::time_t gameOverTimer;
+bool isGameOver = false;
+
+
+struct Particle {
+    float x, y, z;   // Position
+    float dx, dy, dz; // Velocity
+    float r, g, b;    // Color
+    float life;       // Lifetime remaining
+
+    Particle(float x, float y, float z, float dx, float dy, float dz, float r, float g, float b, float life)
+            : x(x), y(y), z(z), dx(dx), dy(dy), dz(dz), r(r), g(g), b(b), life(life) {}
+};
+
+class ParticleSystem {
+public:
+    std::vector<Particle> particles;
+
+    void generateExplosion(float x, float y, float z) {
+        for (int i = 0; i < 100; ++i) {
+            // Randomize direction of particles
+            float dx = static_cast<float>(rand()) / RAND_MAX - 0.5f;
+            float dy = static_cast<float>(rand()) / RAND_MAX - 0.5f;
+            float dz = static_cast<float>(rand()) / RAND_MAX - 0.5f;
+            particles.emplace_back(x, y, z, dx, dy, dz, 1.0f, 0.5f, 0.0f, 1.0f);
+        }
+    }
+
+    void update(float dt) {
+        for (auto &particle: particles) {
+            particle.x += particle.dx * dt;
+            particle.y += particle.dy * dt;
+            particle.z += particle.dz * dt;
+            particle.life -= dt;
+        }
+        particles.erase(std::remove_if(particles.begin(), particles.end(),
+                                       [](const Particle &p) { return p.life <= 0; }),
+                        particles.end());
+    }
+
+    void draw() {
+        glDisable(GL_LIGHTING);
+        glBegin(GL_POINTS);
+        for (auto &particle: particles) {
+            glColor3f(particle.r, particle.g, particle.b);
+            glVertex3f(particle.x, particle.y, particle.z);
+        }
+        glEnd();
+        glEnable(GL_LIGHTING);
+    }
+};
+
+ParticleSystem particleSystem; // Instance of ParticleSystem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Game::Game() = default;
+
+void loadGameOverSound() {
+    PlaySound("..//Sounds//game-over.wav", nullptr, SND_ASYNC);
+}
+
+void loadDeathSound() {
+    PlaySound("..//Sounds//death.wav", nullptr, SND_ASYNC);
+}
+
+void gameOver() {
+    spaceCraft->deactivate();
+    explosionX = spaceCraft->getX();
+    explosionZ = spaceCraft->getZ();
+    isExplosion = true;
+    particleSystem.generateExplosion(0, 0.0, 0);
+    loadGameOverSound();
+    isGameOver = true;
+    cout << "Game Over!!!!!!!!!!!!!!!!!!!!" << endl;
+
+//    gameOverTimer = std::time(nullptr); // get current time
+
+//    std::this_thread::sleep_for(std::chrono::seconds(5)); // wait for 5 seconds
+//    windowChoice = 0;
+//    menu.setup();
+}
+
+tuple<float, float, float> getCurrentCoordinates(int planetNum) {
+
+    vector<int> x = {0, 30, 50, 150, -90, -110, -130, 50, -150, 125};
+    vector<int> z = {-150, -140, -110, -90, -70, -200, -210, -70, -50, -90};
+
+    // Orbital and rotational speeds (relative to some base speed)
+    float orbitalSpeeds[] = {0.0f, 4.74f, 3.50f, 2.98f, 2.41f, 1.31f, 0.97f, 0.68f, 0.54f, 2.98f};
+
+    // Calculate the current position based on the orbital angle
+    float currentAngle = angle * orbitalSpeeds[planetNum] * M_PI / 180.0f;
+    float currentX = x[planetNum] * cos(currentAngle) - z[planetNum] * sin(currentAngle);
+    float currentZ = x[planetNum] * sin(currentAngle) + z[planetNum] * cos(currentAngle);
+    return make_tuple(currentX, 0.0, currentZ);
+}
+
+void checkPlanetCollisionsWithSpacecraft() {
+    for (int i = 0; i < 9; ++i) {
+        Planet &planet = planets[i];
+        auto [planetX, planetY, planetZ] = getCurrentCoordinates(i);
+
+        float distance = sqrt(pow(spaceCraft->getX() - planetX, 2) +
+                              pow(spaceCraft->getZ() - planetZ, 2));
+
+//        cout << "distance : " << distance << endl;
+        if (distance < 10.0) { // Collision threshold
+            // game over
+            cout << "Game Over!" << endl;
+//            deathSound();
+            exit(0);
+        }
+    }
+}
 
 void generatePickables() {
     float x = static_cast<float>(rand() % 20 - 10);
@@ -65,14 +227,20 @@ void updatePickables() {
 
 void checkCollisionWithEnemies() {
     for (auto &enemy: enemies) {
-        if (enemy->isActive()) {
+        if (enemy->isActive() && spaceCraft->isActive()) {
 
             float distance = sqrt(pow(spaceCraft->getX() - enemy->getX(), 2) +
                                   pow(spaceCraft->getY() - enemy->getY(), 2) +
                                   pow(spaceCraft->getZ() - enemy->getZ(), 2));
             if (distance < 4.0f) { // Collision threshold
-                spaceCraft->deactivate();
-                cout << "Game Over!!!!!!!!!!!!!!!!!!!!" << endl;
+//                spaceCraft->deactivate();
+//                explosionX = spaceCraft->getX();
+//                explosionZ = spaceCraft->getZ();
+//                isExplosion = true;
+//                particleSystem.generateExplosion(0, 0.0, 0 );
+//                loadGameOverSound();
+//                cout << "Game Over!!!!!!!!!!!!!!!!!!!!" << endl;
+                gameOver();
             }
         }
 
@@ -85,14 +253,20 @@ void checkProjectileCollisions() {
     for (auto &enemy: enemies) {
         if (enemy->isActive()) {
             for (auto &projectile: enemy->projectiles) {
-                if (projectile.isActive()) {
+                if (projectile.isActive() && spaceCraft->isActive()) {
                     float distance = sqrt(pow(spaceCraft->getX() - projectile.getX(), 2) +
                                           pow(spaceCraft->getY() - projectile.getY(), 2) +
                                           pow(spaceCraft->getZ() - projectile.getZ(), 2));
                     if (distance < 1.0f) { // Collision threshold
                         bool isDied = spaceCraft->takeDamage(10); // Decrease player health
                         if (isDied) {
-                            cout << "Game Over!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+//                            explosionX = spaceCraft->getX();
+//                            explosionZ = spaceCraft->getZ();
+//                            isExplosion = true;
+//                            particleSystem.generateExplosion(0, 0.0, 0 );
+//                            loadGameOverSound();
+//                            cout << "Game Over!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+                            gameOver();
                         }
                         projectile.deactivate(); // Deactivate projectile
                     }
@@ -112,7 +286,14 @@ void checkProjectileCollisions() {
                     if (distance < 2.0f) { // Collision threshold
                         bool isDied = enemy->takeDamage(projectile.getStrength());
                         projectile.deactivate(); // Deactivate projectile
-                        if (isDied) spaceCraft->increaseScore(100); // Increase player score
+                        if (isDied) {
+                            spaceCraft->increaseScore(100); // Increase player score
+                            explosionX = enemy->getX();
+                            explosionZ = enemy->getZ();
+                            isExplosion = true;
+                            particleSystem.generateExplosion(0, 0.0, 0);
+                            loadDeathSound();
+                        }
                         cout << "player attacked enemy" << endl;
                     }
                 }
@@ -312,6 +493,28 @@ void Game::animate(int value = 0) {
     checkCollisionWithEnemies();
     updatePickables();
     checkPickableCollisions();
+
+//    checkPlanetCollisionsWithSpacecraft();
+
+//    if(isExplosion){
+//        explosionRadius += 0.1;
+//        if(explosionRadius > explosionMaxRadius){
+//            isExplosion = false;
+//            explosionRadius = 0.0;
+//        }
+//    }
+
+// Handle explosion animation
+    if (isExplosion) {
+        particleSystem.update(0.016f); // Update particles with a delta time (adjust as needed)
+        explosionRadius += 0.1f; // Increase explosion radius
+        if (explosionRadius > explosionMaxRadius) {
+            isExplosion = false;
+            explosionRadius = 0.0f;
+            particleSystem.particles.clear(); // Clear particles after explosion
+        }
+    }
+
     if (angle > 360) angle = 0;
 
     int currentTime = glutGet(GLUT_ELAPSED_TIME);
@@ -324,77 +527,10 @@ void Game::animate(int value = 0) {
     glutTimerFunc(animationPeriod, animate, value);
 }
 
-
-void solarSystemViewPort(int windowWidth, int windowHeight, int miniMapSize) {
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    gluOrtho2D(0, windowWidth, 0, windowHeight); // Adjust to your window size
-
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-
-    // Disable depth testing for HUD rendering
-    glDisable(GL_DEPTH_TEST);
-
-
-    glLineWidth(2.0);
-    glColor3f(1.0, 1.0, 0.0); // Set color to yellow
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(windowWidth - miniMapSize - 10, 10);
-    glVertex2f(windowWidth - 10, 10);
-    glVertex2f(windowWidth - 10, miniMapSize + 10);
-    glVertex2f(windowWidth - miniMapSize - 10, miniMapSize + 10);
-    glEnd();
-
-
-    // Restore previous state
-    glEnable(GL_DEPTH_TEST);
-
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-
-    glMatrixMode(GL_MODELVIEW);
-    glPopMatrix();
-
-    // end of main viewport
-
-    // Second viewport for mini-map
-    glViewport(windowWidth - miniMapSize - 10, 10, miniMapSize, miniMapSize); // 10 pixel padding
-    glLoadIdentity();
-    glClear(GL_DEPTH_BUFFER_BIT);
-
-    // Set up camera for the mini-map view (top-down view)
-    gluLookAt(
-            0.0, 50.0, -200.0, // Camera position above the origin
-            0.0, 0.0, -150,     // Look-at point at the origin
-            0.0, 1.0, 0.0      // Up vector (looking down)
-    );
-
-
-    // Draw the entire scene for the mini-map view
-    glPushMatrix();
-    glScalef(2, 2, 2);
-    Planet::drawPlanets(planets, angle);
-//    asteriods->draw();
-    drawPickables();
-    // Spacecraft Setup and draw
-    spaceCraft->setEnemy(false);
-    spaceCraft->setup();
-    glPushMatrix();
-    glTranslatef(spaceCraft->getX(), 0.0, spaceCraft->getZ());
-    glRotatef(spaceCraft->getAngle(), 0.0, 1.0, 0.0);
-    spaceCraft->draw();
-    glPopMatrix();
-
-    glPopMatrix();
-
-}
-
 void Game::draw() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    cout << "drawing game" << endl;
 
     int windowWidth = glutGet(GLUT_WINDOW_WIDTH);
     int windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
@@ -467,6 +603,21 @@ void Game::draw() {
     drawPickables();
     // Draw HUD
     drawHUD();
+    // Draw explosion
+    if (isExplosion) {
+//        glPushMatrix();
+//        glTranslatef(explosionX, 0.0, explosionZ);
+//        glColor3f(1.0, 0.0, 0.0);
+//        glutSolidSphere(explosionRadius, 20, 20);
+//        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(explosionX, 0.0, explosionZ);
+        glScalef(50, 50, 50);
+        glPointSize(5);
+        particleSystem.draw();
+        glPopMatrix();
+    }
 
     glPopMatrix();
 
@@ -504,6 +655,17 @@ void Game::draw() {
     glMatrixMode(GL_MODELVIEW);
     glPopMatrix();
 
+
+//    std::time_t currentTime = std::time(nullptr);
+//
+//    // loop until 5 seconds have passed
+//    if (isGameOver && std::difftime(currentTime, gameOverTimer) >= 5){
+//        windowChoice = 0;
+//        glViewport(0, 0, windowWidth, windowHeight); // Adjust to the full window size
+//        menu.setup();
+//        menu.draw();
+//    }else{
+
     // end of main viewport
 
     // Second viewport for mini-map
@@ -535,6 +697,9 @@ void Game::draw() {
     glPopMatrix();
 
     glPopMatrix();
+//    }
+
+
 
 
     glutSwapBuffers();
@@ -633,3 +798,4 @@ void Game::specialKeyInput(int key, int x, int y) {
 
     glutPostRedisplay();
 }
+
